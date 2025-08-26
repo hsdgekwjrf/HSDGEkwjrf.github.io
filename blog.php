68fd4c9a0644f47d90abe4b3e9a6c1e58c94b83f
888666444 <95750947+hsdgekwjrf@users.noreply.github.com>
2025-08-26 11:38:05 +0800

    Update blog.php

--- blog.php
+++ blog.php
@@ -1,8 +1,9 @@
 <?php
 	echo "Uploading...";
 	$file=fopen("./blog/".$_POST["name"],"x") or exit("Error!\nExit.");
-	echo file_put_contents("./blog/".$_REQUEST["name"],$_REQUEST["code"]);
+	echo file_put_contents("./blog/".$_POST["name"],$_POST["code"]);
 	fclose($file);
 	exit("\nExit.");
 
 ?>
+
